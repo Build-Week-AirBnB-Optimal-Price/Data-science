1cbf2822e1eb2516257107ea0a05693a1a9cb8cb
lechemrc <roblecheminant@gmail.com>
2019-12-18 22:16:14 -0700

    heroku deployment master

--- airbnb/app.py
+++ airbnb/app.py
@@ -1,16 +1,12 @@
-[from decouple import config
+from decouple import config
 from flask import Flask, render_template, request
+# from .models import DB
 
-from .models import DB
+app = Flask(__name__)
+# APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# DB = SQLAlchemy(APP)
 
-def create_app():
-    """Create and configure an instance of the Flask application"""
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    DB.init_app(app)
-
-    @app.route('/')
-    def root():
-        DB.create_all()
-        return render_template('base.html', title='Home')
+@app.route('/')
+def root():
+    # DB.create_all()
+    return render_template('base.html', title='Home')
