34f20fb03b73da0f6b3d386b14ff326d14969f52
lechemrc <roblecheminant@gmail.com>
2019-12-19 15:51:23 -0700

    moving and updating files

--- airbnb/app.py
+++ airbnb/app.py
@@ -2,11 +2,18 @@ from decouple import config
 from flask import Flask, render_template, request
 # from .models import DB
 
-app = Flask(__name__)
-# APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///airbnb_db.sqlite3'
+# app = Flask(__name__)
+# APP.config['SQLALCHEM Y_DATABASE_URI'] = 'sqlite:///airbnb_db.sqlite3'
 # DB = SQLAlchemy(APP)
 
-@app.route('/')
-def root():
-    # DB.create_all()
-    return render_template('base.html', title='Home')
+def create_app():
+    """Create and configure an instance of the Flask application"""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        # DB.create_all()
+        return render_template('base.html', title='Home')
