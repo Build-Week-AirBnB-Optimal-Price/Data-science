49f52af2a566881ec4c9742bfbd56d926491e274
lechemrc <roblecheminant@gmail.com>
2020-01-07 21:07:39 -0700

    updating app.py

--- airbnb_src/app.py
+++ airbnb_src/app.py
@@ -3,6 +3,23 @@ from flask import Flask, render_template
 
 from .models import DB
 
+# create df
+train = pd.read_csv('train.csv') # change file path
+# drop null values
+train.dropna(inplace=True)
+# features and target
+target = 'Survived'
+features = ['Pclass', 'Age', 'SibSp', 'Fare']
+# X matrix, y vector
+X = train[features]
+y = train[target]
+# model
+model = LogisticRegression()
+model.fit(X, y)
+model.score(X, y)
+​
+pickle.dump(model, open('model.pkl', 'wb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 def create_app():
     """Create and configure an instance of the Flask application"""
@@ -15,5 +32,22 @@ def create_app():
     def root():
         DB.create_all()
         return render_template('base.html', title='Home')
+
+    def predict():
+        # get data
+        data = request.get_json(force=True)
+
+        # convert data into dataframe
+        data.update((x, [y]) for x, y in data.items())
+        data_df = pd.DataFrame.from_dict(data)
+
+        # predictions
+        result = model.predict(data_df)
+
+        # send back to browser
+        output = {'results': int(result[0])}
+
+        # return data
+        return jsonify(results=output)
 
     return app
