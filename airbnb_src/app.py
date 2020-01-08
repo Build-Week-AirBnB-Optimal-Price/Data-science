0b3dcbcf27371b8f642d7d0965cf0c9fb2efe62b
lechemrc <roblecheminant@gmail.com>
2020-01-07 21:14:11 -0700

    updating app.py, delete init

--- airbnb_src/app.py
+++ airbnb_src/app.py
@@ -1,7 +1,9 @@
 from decouple import config
-from flask import Flask, render_template
-
-from .models import DB
+import pandas as pd
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sklearn.linear_model import LogisticRegression
+import pickle
 
 # create df
 train = pd.read_csv('train.csv') # change file path
@@ -17,37 +19,31 @@ y = train[target]
 model = LogisticRegression()
 model.fit(X, y)
 model.score(X, y)
-​
+
 pickle.dump(model, open('model.pkl', 'wb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-def create_app():
-    """Create and configure an instance of the Flask application"""
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    DB.init_app(app)
 
-    @app.route('/')
-    def root():
-        DB.create_all()
-        return render_template('base.html', title='Home')
+"""Create and configure an instance of the Flask application"""
+app = Flask(__name__)
 
-    def predict():
-        # get data
-        data = request.get_json(force=True)
+@app.route('/', methods = ['GET', 'POST'])
+def predict():
+    # get data
+    data = request.get_json(force=True)
 
-        # convert data into dataframe
-        data.update((x, [y]) for x, y in data.items())
-        data_df = pd.DataFrame.from_dict(data)
+    # convert data into dataframe
+    data.update((x, [y]) for x, y in data.items())
+    data_df = pd.DataFrame.from_dict(data)
 
-        # predictions
-        result = model.predict(data_df)
+    # predictions
+    result = model.predict(data_df)
 
-        # send back to browser
-        output = {'results': int(result[0])}
+    # send back to browser
+    output = {'results': int(result[0])}
 
-        # return data
-        return jsonify(results=output)
+    # return data
+    return jsonify(results=output)
 
-    return app
+if __name__ == '__main__':
+    app.run()
