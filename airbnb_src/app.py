954398e0d8a6301b1e7cdce15b19d50ef9a3895b
lechemrc <roblecheminant@gmail.com>
2020-01-09 20:27:45 -0700

    uploading refined app and new model page

--- airbnb_src/app.py
+++ airbnb_src/app.py
@@ -1,60 +1,24 @@
 import pandas as pd
+import os
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-import category_encoders as ce
+from joblib import load
+from flask import Flask, jsonify, request
 import pickle
 
-# create df
-trainval = pd.read_csv('final3_airbnb.csv')  # change file path
-
-# drop null values
-trainval.dropna(inplace=True)
-
-# setting features and target
-target = 'price'
-features = ['host_since', 'zipcode', 'room_type', 'maximum_nights', 'minimum_nights',
-            'extra_people', 'accommodates', 'neighbourhood','beds', 'property_type',
-            'cancellation_policy', 'guests_included', 'bedrooms', 'bathrooms']
-
-
-def wrangle(X):
-    X = X.copy()
-    return X
-
-
-train_rentals, val_rentals = train_test_split(trainval, random_state=42)
-train = wrangle(train_rentals)
-val = wrangle(val_rentals)
-X_train = train.drop(columns=target)
-X_val = val.drop(columns=target)
-y_train = train[target]
-y_val = val[target]
-
-# X = train[features]
-# y = train[target]
-
-# model
-model = make_pipeline(
-    ce.OrdinalEncoder(),
-    RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
-)
-
-model.fit(X_train, y_train)
-model.predict(X_val)
-
-pickle.dump(model, open('model.pkl', 'wb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
-DB = SQLAlchemy()
+model_path = os.path.join('airbnb_src', 'model.pkl')
+print(model_path)
+model = load(model_path)
+print('reading model')
 
 """Create and configure an instance of the Flask application"""
 app = Flask(__name__)
 
+@app.route('/')
+def root():
+    return 'Hello World'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
