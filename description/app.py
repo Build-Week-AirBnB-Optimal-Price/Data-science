ddb0bda0a3c74fd7743062f35595612aeae9b294
AnikaZN <49991173+AnikaZN@users.noreply.github.com>
2020-01-10 19:08:07 -0700

    Upload clean app.py

--- description/app.py
+++ description/app.py
@@ -8,31 +8,25 @@ import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# def sentiment_analysis(input):
-#     sid = SentimentIntensityAnalyzer()
-#     scores = sid.polarity_scores(input)
-#     rating = scores.get('compound')
-#     return rating
-
+# Model
 sid = SentimentIntensityAnalyzer()
 
+# Pickle it
 pickle.dump(sid, open('model.pkl', 'wb'))
-
 model = pickle.load(open('model.pkl', 'rb'))
 
+# Create App
 app = Flask(__name__)
-
-# @app.route('/')
-# def root():
-#     return render_template('base.html', methods = ['POST'])
 
 @app.route('/', methods = ['GET', 'POST'])
 def analysis():
 
+    # Take JSON input
     text = request.get_json(force = True)
+    # Run JSON as text through model
     prediction = model.polarity_scores(str(text))
-    # output = prediction.get('compund')
 
+    # Re-convert results to JSON
     return jsonify(results = prediction)
 
 if __name__ == '__main__':
